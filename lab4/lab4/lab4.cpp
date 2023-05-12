e6a82b314fc28ef40e6e2a8d9b2fac81114c140a
tsukerin <107303964+tsukerin@users.noreply.github.com>
2023-05-13 01:01:12 +0300

    Update lab4.cpp

--- lab4/lab4/lab4.cpp
+++ lab4/lab4/lab4.cpp
@@ -1,6 +1,45 @@
 ﻿#include <iostream>
 
+int countArray(int x[], int n) {
+    int counter = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (x[i] % 7 == 0) { counter++; }
+    }
+
+    return counter;
+}
+
+using namespace std;
+
 int main()
 {
-    int matrix = new int[*][*];
+    int n = 0;
+
+    cout << "Enter value of elements array A: ";
+    cin >> n;
+    cout << "Enter elements of array A: \n";
+
+    int* a = new int[n];
+
+    for (int i = 0; i < n; i++) {
+        cout << "A[" << i + 1 << "]= ";
+        cin >> a[i];
+    }
+
+    cout << "Value of elements array A, dividing without remains: " << countArray(a, n) << "\n";
+    cout << "Enter value of elements array B: ";
+    cin >> n;
+    cout << "Enter elements of array B: \n";
+
+    int* b = new int[n];
+
+    for (int i = 0; i < n; i++) {
+        cout << "B[" << i + 1 << "]= ";
+        cin >> b[i];
+    }
+
+    cout << "Value of elements array B, dividing without remains: " << countArray(b, n);
+
+    return 0;
 }
